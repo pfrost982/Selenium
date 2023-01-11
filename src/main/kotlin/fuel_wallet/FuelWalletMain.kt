57780564c369aadcb5b880cfb4f3fc9bea100132
pfrost982 <pfrost982@gmail.com>
2023-01-11 23:57:39 +0700

    real work

--- src/main/kotlin/fuel_wallet/FuelWalletMain.kt
+++ src/main/kotlin/fuel_wallet/FuelWalletMain.kt
@@ -11,7 +11,7 @@ var profileWork = false
 var isError = false
 
 fun main() = runBlocking {
-    val profiles = listOf<Int>(2)// + (138 .. 149)
+    val profiles = listOf<Int>() + (5 .. 150)
     for (number in profiles) {
         if (isError){
             break
