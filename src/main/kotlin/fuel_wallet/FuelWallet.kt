e144122437cfdb62eb685760c1028cd8bafd5236
pfrost982 <pfrost982@gmail.com>
2023-01-11 15:50:57 +0700

    fuel_wallet

---
+++ src/main/kotlin/fuel_wallet/FuelWallet.kt
@@ -0,0 +1,60 @@
+package fuel_wallet
+
+import ads_std.*
+import org.sikuli.script.*
+
+suspend fun fuelScript(number: Int) {
+    val seed = Seeds.getSeed(number)
+    val driver = openProfile(number)
+    println("profile $number: start script on thread ${Thread.currentThread().name}")
+    closeAllTabs(driver)
+    driver.manage().window().maximize()
+    driver.get("chrome-extension://keomgfjjdfafellomeiicgdkjnedoadc/index.html#/sign-up/welcome")
+    val screen = Screen()
+    ImagePath.add("src/main/kotlin/fuel_wallet/png")
+    try {
+        screen.wait("i_have_wallet.png", 7.0)
+        screen.click()
+        screen.wait(Pattern("will_need_next_step.png").similar(0.9).targetOffset(-110, 65))
+        screen.click()
+        insertTextTroughClipboard(screen, seed)
+        screen.wait("next.png")
+        screen.click()
+        screen.wait("type_password.png")
+        screen.click()
+        insertTextTroughClipboard(screen, WALLET_PASS)
+        screen.wait("confirm_password.png")
+        screen.click()
+        insertTextTroughClipboard(screen, WALLET_PASS)
+        screen.wait(Pattern("i_agree.png").targetOffset(-55, 0))
+        screen.click()
+        screen.wait("next.png")
+        screen.click()
+        screen.wait("wallet_created.png", 10.0)
+        screen.wait("extensions.png")
+        screen.click()
+        screen.wait("fuel_wallet.png")
+        screen.click()
+/*
+        screen.wait("faucet.png", 7.0)
+        screen.click()
+*/
+        driver.get("https://fuels-wallet.vercel.app/docs/how-to-use/")
+        screen.wait("connect.png", 7.0)
+        screen.click()
+
+
+    } catch (e: FindFailed) {
+        e.printStackTrace()
+        isError = true
+    }
+    profileWork = false
+
+    isError = true //
+
+    if (isError) {
+        driver.quit()
+    } else {
+        closeProfile(number, driver)
+    }
+}
---
+++ src/main/kotlin/fuel_wallet/FuelWalletMain.kt
@@ -0,0 +1,32 @@
+package fuel_wallet
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+@Volatile
+var profileWork = false
+@Volatile
+var isError = false
+
+fun main() = runBlocking {
+    val profiles = listOf<Int>(1)// + (138 .. 149)
+    for (number in profiles) {
+        if (isError){
+            break
+        }
+        profileWork = true
+        launch(Dispatchers.Default) {
+            fuelScript(number)
+        }
+        while (profileWork) {
+            delay(1000)
+        }
+    }
+    if(isError) {
+        println("Work ended, with error!")
+    } else {
+        println("Work ended!")
+    }
+}
