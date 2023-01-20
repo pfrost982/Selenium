da9fad879d0cd4c80d62f341a30069727a5e1ea1
pfrost982 <pfrost982@gmail.com>
2023-01-20 07:34:18 +0700

    Kresko norm work

--- src/main/kotlin/kresko/Kresko.kt
+++ src/main/kotlin/kresko/Kresko.kt
@@ -23,41 +23,62 @@ suspend fun kreskoScript(number: Int) {
         closeAllTabs(driver)
         driver.get("https://www.kresko.fi/")
         screen.wait(1.0)
-        screen.wait("not_robot.png")
-        screen.click()
         screen.wait("email_input.png")
         screen.click()
         insertTextTroughClipboard(screen, mail)
         screen.wait("name_input.png")
         screen.click()
         insertTextTroughClipboard(screen, name)
-        screen.wait(Pattern("right_arrow.png").similar(0.95), 180.0)
+        screen.wait("not_robot.png")
+        screen.click()
+        screen.mouseMove(50, 0)
+        var arrow = screen.exists(Pattern("right_arrow.png").similar(0.95), 180.0)
+        while (arrow == null) {
+            driver.get("https://www.kresko.fi/")
+            screen.wait(1.0)
+            screen.wait("email_input.png")
+            screen.click()
+            insertTextTroughClipboard(screen, mail)
+            screen.wait("name_input.png")
+            screen.click()
+            insertTextTroughClipboard(screen, name)
+            screen.wait("not_robot.png")
+            screen.click()
+            screen.mouseMove(50, 0)
+            arrow = screen.exists(Pattern("right_arrow.png").similar(0.95), 180.0)
+        }
         screen.click()
         screen.wait(1.0)
         driver.get("https://mail.rambler.ru/folder/INBOX")
-        val pochta = screen.exists("pochta.png")
+        val pochta = screen.exists(Pattern("pochta.png").similar(0.9), 9.0)
+        println(pochta)
         if (pochta != null) {
+            println("profile $number: mail input case ${Thread.currentThread().name}")
             screen.click()
             screen.wait("planet.png")
             screen.click()
             screen.wait("voyti.png")
             screen.click()
+        } else {
+            println("profile $number: in mail case ${Thread.currentThread().name}")
         }
         screen.wait(2.0)
+        screen.wait(Pattern("spam.png").similar(0.9), 10.0)
+        screen.wait(0.7)
         checkNotifications(screen)
-        screen.wait("spam.png")
+        screen.wait(Pattern("spam.png").similar(0.9))
         screen.click()
-        screen.wait("kresko_mail_title.png")
+        screen.wait("kresko_mail_title.png", 5.0)
         screen.click()
         screen.wait("vklyuchit_ssilki.png")
         screen.click()
+        screen.wait(Pattern("confirm_waitlist.png").similar(0.95))
+        screen.click()
+        screen.wait(5.0)
     } catch (e: FindFailed) {
         e.printStackTrace()
         isError = true
     }
-
-    isError = true
-
     profileWork = false
     if (isError) {
         driver.quit()
