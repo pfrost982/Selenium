72fbba3567dc8f9471dd954c7824f76c1cec64fc
pfrost982 <pfrost982@gmail.com>
2023-05-23 23:32:41 +0700

    monstercap work

--- src/main/kotlin/cap_monster/CapMonsterQueueMain.kt
+++ src/main/kotlin/cap_monster/CapMonsterQueueMain.kt
@@ -1,6 +1,7 @@
-package scroll
+package cap_monster
 
 import ads_std.formWorkingRegions
+import ads_std.queueCloseProfileReleaseWorkRegion
 import ads_std.queueOpenProfile
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -8,15 +9,18 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.sikuli.script.ImagePath
 
-val errorListQ = mutableListOf<Int>()
+var backgroundGreen = "\u001B[42m"
+var backgroundBlack = "\u001B[40m"
+val backgroundRed = "\u001B[41m"
+val errorList = mutableListOf<Int>()
 fun main(): Unit = runBlocking {
-    ImagePath.add("src/main/kotlin/scroll/png")
-    val list = listOf<Int>(1)// + (11..20)
+    ImagePath.add("src/main/kotlin/cap_monster/png")
+    val list = listOf<Int>() + (1..27)
     val profiles = list.toMutableList()
     println("Profiles:\n$profiles")
     val freeWorkRegions = formWorkingRegions(
-        2, 2, 0, 0, 900, 1060,
-        screenAdditionalWidth = 540
+        3, 3, 0, 0, 690, 690, 10, 10,
+        screenAdditionalWidth = 0
     )
     while (profiles.isNotEmpty()) {
         if (freeWorkRegions.isNotEmpty()) {
@@ -25,10 +29,12 @@ fun main(): Unit = runBlocking {
             }
             launch(Dispatchers.Default) {
                 queueOpenProfile(region)
-                poolOutScript(region)
-                //queueCloseProfileReleaseWorkRegion(region, freeWorkRegions)
+                addKeyScript(region)
+                queueCloseProfileReleaseWorkRegion(region, freeWorkRegions)
+                println(backgroundRed + "Error list:" + backgroundBlack)
+                println(backgroundRed + errorList + backgroundBlack)
             }
-            println("Error list:\n$errorListQ")
+
         }
         delay(500)
     }
--- src/main/kotlin/scroll/ScrollQueueScript.kt
+++ src/main/kotlin/scroll/ScrollQueueScript.kt
@@ -2,71 +2,40 @@ package scroll
 
 import ads_std.*
 import org.sikuli.script.FindFailed
-import org.sikuli.script.Key
-import org.sikuli.script.Pattern
 
 suspend fun poolOutScript(workRegion: WorkRegion) {
     val screen = workRegion.screen
-    println("Start profile ${workRegion.profile}, line: ${workRegion.line}, row: ${workRegion.row}")
+    var backgroundColor = "\u001B[42m"
+    println("$backgroundColor + Start profile ${workRegion.profile}, line: ${workRegion.line}, row: ${workRegion.row}")
     try {
-        var metamaskLanguage: String
         openUrlSikuliDark(screen, "https://staging.syncswap.xyz/pool")
-        val start = screen.exists("sync_swap_start_button.png", 6.0)
-        if (start != null) {
-            println("Start button case")
-            screen.queueTakeClick()
-            screen.type(Key.ESC)
-            screen.queueRelease()
-            screen.wait(Pattern("sync_swap_connect_wallet.png").targetOffset(-136, 0))
-            screen.queueTakeClickRelease()
-            screen.wait("sync_swap_scroll_button.png")
-            screen.queueTakeClickRelease()
-            screen.wait("sync_swap_connect_wallet.png")
-            screen.queueTakeClickRelease()
-            screen.wait("sync_swap_metamask_button.png")
-            screen.queueTakeClickRelease()
-            metamaskLanguage = metamaskUnlock(screen)
-            println("Metamask language = $metamaskLanguage")
-            //metamaskGotIt(screen)
-            metamaskNext(screen, metamaskLanguage)
-            metamaskConnect(screen, metamaskLanguage)
-            screen.wait("sync_swap_try_unlock_button.png")
-            screen.queueTakeClickRelease()
-        }
-
-        metamaskLanguage = metamaskUnlock(screen)
+        val metamaskLanguage = metamaskUnlock(screen)
         println("Metamask language = $metamaskLanguage")
         screen.wait(2.5)
-        if (metamaskIsOpened(screen, 4.0)) {
-            metamaskScroll(screen, 3, delayBeforeScroll = 1.0)
-            screen.wait(0.5)
-            metamaskReject(screen, metamaskLanguage)
-            metamaskNext(screen, metamaskLanguage)
-            metamaskConnect(screen, metamaskLanguage)
+        if (metamaskIsOpenedDark(screen)) {
+            metamaskSwitchNetwork(screen, metamaskLanguage)
+        }
+
+        metamaskGotIt(screen)
+
+        screen.wait("sync_swap_connect_wallet.png")
+        screen.queueTakeClickRelease()
+        screen.wait("sync_swap_metamask_button.png")
+        screen.queueTakeClickRelease()
+
+        val switch = screen.exists("sync_swap_switch_network_button.png", 4.0)
+        if (switch != null) {
+            println("Switch case")
+            screen.queueTakeClickRelease()
             metamaskAllowAddNetwork(screen, metamaskLanguage)
             metamaskSwitchNetwork(screen, metamaskLanguage)
         } else {
-
-            metamaskGotIt(screen)
-
-            screen.wait("sync_swap_connect_wallet.png")
-            screen.queueTakeClickRelease()
-            screen.wait("sync_swap_metamask_button.png")
-            screen.queueTakeClickRelease()
-
-            val switch = screen.exists("sync_swap_switch_network_button.png", 4.0)
-            if (switch != null) {
-                println("Switch case")
-                screen.queueTakeClickRelease()
-                metamaskAllowAddNetwork(screen, metamaskLanguage)
-                metamaskSwitchNetwork(screen, metamaskLanguage)
-            } else {
-                println("No switch case")
-            }
+            println("No switch case")
         }
     } catch (e: FindFailed) {
         e.printStackTrace()
-        isError = true
+        backgroundColor = "\u001B[41m"
+        errorListQ.add(workRegion.profile)
     }
-    println("Finish profile ${workRegion.profile}, line: ${workRegion.line}, row: ${workRegion.row}")
+    println(backgroundColor + "Finish profile ${workRegion.profile}, line: ${workRegion.line}, row: ${workRegion.row}")
 }
