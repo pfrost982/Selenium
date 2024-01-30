8094ed5d03b7a92d69eed09afb26ee2d6e332c2e
pfrost982 <pfrost982@gmail.com>
2024-01-31 03:07:05 +0700

    discords

--- src/main/kotlin/discord/DiscordAccounts.kt
+++ src/main/kotlin/discord/DiscordAccounts.kt
@@ -3,7 +3,7 @@ package discord
 import java.io.File
 
 object DiscordAccounts {
-    private val tokens: List<String> = File("src/main/kotlin/ads_std/accounts.txt").useLines { it.toList() }
+    private val tokens: List<String> = File("src/main/kotlin/discord/accounts.txt").useLines { it.toList() }
     fun getToken(number: Int): String {
         return tokens[number - 1]
     }
